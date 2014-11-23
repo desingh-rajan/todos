6a59ac88f9a8ce48c837f04e590dc515f39ae7d3
Desingh <desinghrajan@brocode-inc.com>
2014-11-23 13:27:33 +0530

    Created todo templates with static data

---
+++ client/templates/todo/todo_list.js
@@ -0,0 +1,12 @@
+/**
+ * Created by desingh on 11/23/14.
+ */
+var todoData = [
+    {title: 'Buy detergent power'},
+    {title: 'Wash your cloths, Almost two weeks gone'},
+    {title: 'Go outside and see the sky, ground, tree! You noob prisoner.'}
+];
+
+Template.todoList.helpers({
+    todo: todoData
+});
