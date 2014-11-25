002806bdc9529fd773bc3fe5de3cba647937dcc5
Desingh <desinghrajan@brocode-inc.com>
2014-11-25 22:43:04 +0530

    Get rid of static todo

--- client/templates/todo/todo_list.js
+++ client/templates/todo/todo_list.js
@@ -1,9 +1,5 @@
-var todoData = [
-    {title: 'Buy detergent power.', description: 'Ayyo mudiyala'},
-    {title: 'Wash your cloths, Almost two weeks gone.'},
-    {title: 'Go outside and see the sky, ground, tree! You noob prisoner.'}
-];
-
 Template.todoList.helpers({
-    todo: todoData
+    todo: function(){
+        return Todo.find();
+    }
 });
